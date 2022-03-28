ee2118e3ccfabf46b7ad52ba3664e8a71414bd5a
Andy Oneal <andy.oneal@andys-macbook-pro.local>
2022-03-28 17:25:01 +0000

    bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as d:
 
 setuptools.setup(
 	 name='pychorusai',  
-	 version='0.2.1',
+	 version='0.2.2',
 	 author="Andy O'Neal",
 	 author_email="andyoneal@me.com",
 	 license="MIT",
